cedb348a50b900ba26d25bef0c531fce11455833
Nayan Thapa <thapa.nyn980@gmail.com>
2024-07-05 14:42:40 -0400

    Update FirebaseDB.js

--- FirebaseDB.js
+++ FirebaseDB.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDU4zz__uD6PLhnRaPn5xofdrO6yT5Vah0",
   authDomain: "advwebdev-dbf13.firebaseapp.com",
@@ -17,7 +12,6 @@ const firebaseConfig = {
   measurementId: "G-Q6VPJ2J1TX"
 };
 
-// Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
